3ffcd943f24eba12a660680f18c02d0495c6bc83
Ronald-Woods <ronwoods@my365.bellevue.edu>
2025-09-21 12:57:00 -0500

    DB Reply 1 Uploaded

---
+++ week1/Module2-dbReply1/Module2-dbReply1/Module2-dbReply1.cpp
@@ -0,0 +1,22 @@
+// Ronald Woods
+// CIS310 - T301
+// 09/21/2025
+
+// Module 2 DB Reply 1
+// Original program written by Charles Edge
+
+// Charles Edge
+// CS310-T301-2261
+// M2: Discussion
+
+#include <iostream>
+using namespace std;
+int main() {
+    int year; //sets integer to year
+    cout << "Enter the current year in YYYY format: " << endl; //prompts for year
+    cin >> year; //stores entered value as year
+
+    cout << "The current year is: " << year << endl; //print to screen
+
+    return 0; //exit
+}
