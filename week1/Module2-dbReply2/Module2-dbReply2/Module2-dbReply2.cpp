fc4cd801f87716acfe6253a291f0de8e9faa593a
Ronald-Woods <ronwoods@my365.bellevue.edu>
2025-09-21 13:10:04 -0500

    DB Reply 2 Uploaded

---
+++ week1/Module2-dbReply2/Module2-dbReply2/Module2-dbReply2.cpp
@@ -0,0 +1,17 @@
+// Ronald Woods
+// CIS310 - T301
+// 09/21/2025
+// This program was written by Siddhant Subedi, my job is to debug.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Please enter your name: ";
+    cin >> name;
+
+    cout << "Hello, " << name << endl;
+
+        return 0;
+}
