c022c19d9d4554c6fe41cb99ecd77c6447f593bc
Ronald-Woods <ronwoods@my365.bellevue.edu>
2025-10-23 13:11:55 -0500

    Module 7 - DB Uploaded

---
+++ weekSeven/moduleSevenDB/moduleSevenDB/moduleSevenDB.cpp
@@ -0,0 +1,22 @@
+//  Ronald Woods
+//  CS301 - T301
+//  10/23/2025
+//  Module 7 Discussion Board Post - 2D Array With Errors
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[2][3] = { {1, 2, 3}, {4, 5, 6} };
+
+    cout << "First element: " << numbers[0][0] << endl;
+
+    for (int i = 0; i < 2; i++ {
+        for (int j = 0; j < 3; j++) {
+            cout << numbers[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return ;
+}
