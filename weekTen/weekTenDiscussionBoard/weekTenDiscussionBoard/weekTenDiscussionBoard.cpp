c72dfb9616d62476c3a7b8fc621f1da158bff727
Ronald-Woods <ronwoods@my365.bellevue.edu>
2025-11-14 20:28:53 -0600

    Week 10 Discussion Board Post - Uploaded

---
+++ weekTen/weekTenDiscussionBoard/weekTenDiscussionBoard/weekTenDiscussionBoard.cpp
@@ -0,0 +1,20 @@
+//  Ronald Woods
+//  CS310 - T301
+//  11/14/2025
+// 
+//  Week 10 - Discussion Board Program - Using Pointers, must contain two errors and not compile. 
+
+#include <iostream>
+using namespace std; 
+
+int main() {
+	int value = 25;
+	int* ptr = &value;
+
+	cout << "Value: " << *ptr << endl;
+	cout << "Value again: " << *prt << endl;
+	cout << "Another value: " << *value << endl;
+
+	return 0;
+}
+
